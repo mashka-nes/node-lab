4f09b11b234d01c19692d5d34041e5ef447bb013
mashka-nes <marie_nes@yahoo.com>
2020-02-10 18:21:59 -0500

    initial

---
+++ quotes.js
@@ -0,0 +1,13 @@
+let quotes = [
+"Be yourself; everyone else is already taken.",
+"Outside of a dog, a book is a mans best friend. Inside of a dog its too dark to read.",
+"Your vibes attract your tribes.",
+"Any negativity belongs to whoever brought, until you accept it.",
+"When you learn how much you are worth, you will stop giving people discounts.",
+"There is nothing better than a friend, unless it is a friend with a chocolate.",
+"Do things for others not because of who you are or what they do in return, but because of who you are."
+];
+
+module.export = {
+    quotes
+};
---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const http = require("http");
+const quotes = require("./quotes");
+
+function newQuote() {
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
+   };
+http.createServer((req, res) => {
+res.writeHead(200, {"Content-type": "text/string"});
+res.write(newQuote());
+res.end();
+}).listen(3000);
